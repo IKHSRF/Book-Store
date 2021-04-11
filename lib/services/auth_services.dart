f961fe62b8f12b6369dbb040c7f6e74b2c3d32e3
Ikhsan Arfian Nugraha <ikhsan3568@gmail.com>
2021-04-11 19:46:54 +0700

    add firebase auth services

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,5 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 class AuthServices {
-  static Future<String> registerUser() async {
-    return 'kosong';
+  static FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  static Future<String> registerUserWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return 'berhasil';
+    } catch (error) {
+      print(error);
+      return error.message;
+    }
+  }
+
+  static Future<String> loginWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return 'berhasil';
+    } catch (error) {
+      print(error);
+      return error.message;
+    }
   }
 }
--- lib/services/storage_services.dart
+++ lib/services/storage_services.dart
@@ -1,1 +1,5 @@
-class StorageServices {}
+import 'package:firebase_storage/firebase_storage.dart';
+
+class StorageServices {
+  static FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
+}
