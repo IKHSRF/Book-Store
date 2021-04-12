7e055fa9aa37074dc90b8c7946128f57dd69784c
Ikhsan Arfian Nugraha <ikhsan3568@gmail.com>
2021-04-12 15:19:16 +0700

    add custom text theme

---
+++ lib/common/textTheme.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextTheme textTheme = TextTheme(
+  headline1: GoogleFonts.sourceSansPro(
+    fontSize: 104,
+    fontWeight: FontWeight.w300,
+    letterSpacing: -1.5,
+  ),
+  headline2: GoogleFonts.sourceSansPro(
+    fontSize: 65,
+    fontWeight: FontWeight.w300,
+    letterSpacing: -0.5,
+  ),
+  headline3: GoogleFonts.sourceSansPro(
+    fontSize: 52,
+    fontWeight: FontWeight.w400,
+  ),
+  headline4: GoogleFonts.sourceSansPro(
+    fontSize: 37,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.25,
+  ),
+  headline5: GoogleFonts.sourceSansPro(
+    fontSize: 26,
+    fontWeight: FontWeight.w400,
+  ),
+  headline6: GoogleFonts.sourceSansPro(
+    fontSize: 22,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0.15,
+  ),
+  subtitle1: GoogleFonts.sourceSansPro(
+    fontSize: 17,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.15,
+  ),
+  subtitle2: GoogleFonts.sourceSansPro(
+    fontSize: 15,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0.1,
+  ),
+  bodyText1: GoogleFonts.raleway(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.5,
+  ),
+  bodyText2: GoogleFonts.raleway(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.25,
+  ),
+  button: GoogleFonts.raleway(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 1.25,
+  ),
+  caption: GoogleFonts.raleway(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.4,
+  ),
+  overline: GoogleFonts.raleway(
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 1.5,
+  ),
+);
