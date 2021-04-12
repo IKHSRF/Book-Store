d3857d2c151f8a5cbe585be57fad22021e1ff2c3
Ikhsan Arfian Nugraha <ikhsan3568@gmail.com>
2021-04-12 13:27:02 +0700

    add firebase storage services

--- lib/pages/admin/distributor/distributor_page.dart
+++ lib/pages/admin/distributor/distributor_page.dart
@@ -1,35 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:toko_buku/providers/distributor_provider.dart';
+import 'package:toko_buku/services/storage_services.dart';
 
 class DistributorPage extends StatelessWidget {
   static const String id = '/distributor';
   @override
   Widget build(BuildContext context) {
-    final distributorProvider = Provider.of<DistributorProvider>(context);
     return Scaffold(
       body: SizedBox.expand(
-        child: StreamBuilder(
-          stream: distributorProvider.distributor,
-          builder: (context, snapshot) {
-            return ListView.builder(
-              itemCount: snapshot.data.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  trailing: Icon(Icons.edit),
-                  title: Text(snapshot.data[index].namaDistributor),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(snapshot.data[index].alamat),
-                      Text(snapshot.data[index].telepon),
-                    ],
-                  ),
-                  isThreeLine: true,
-                );
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextButton(
+              child: Text("Image Database"),
+              onPressed: () async {
+                var file = await StorageServices.getImage();
+                StorageServices.uploadImage(file);
               },
-            );
-          },
+            ),
+          ],
         ),
       ),
     );
--- lib/services/storage_services.dart
+++ lib/services/storage_services.dart
@@ -1,5 +1,28 @@
 import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:image_picker/image_picker.dart';
 
 class StorageServices {
   static FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
+  static ImagePicker _picker = ImagePicker();
+
+  static Future<String> uploadImage(File imageFile) async {
+    try {
+      String filename = basename(imageFile.path);
+      Reference ref = _firebaseStorage.ref().child(filename);
+      Task task = ref.putFile(imageFile);
+      TaskSnapshot taskSnapshot = await task;
+
+      return await taskSnapshot.ref.getDownloadURL();
+    } catch (error) {
+      print(error);
+      return error.message;
+    }
+  }
+
+  static Future<File> getImage() async {
+    final pickedFile = await _picker.getImage(source: ImageSource.gallery);
+    return File(pickedFile.path);
+  }
 }
